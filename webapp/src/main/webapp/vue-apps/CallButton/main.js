75a7fe2ab544a69980614a2cb9a4cea4a904bbf4
Peter Nedonosko <pnedonosko@exoplatform.com>
2020-12-09 15:12:20 +0200

    Add more comments about call state flow in notification toast

--- webapp/src/main/webapp/vue-apps/CallButton/main.js
+++ webapp/src/main/webapp/vue-apps/CallButton/main.js
@@ -63,6 +63,7 @@ export function init(settings) {
 }
 
 export function updateCallState(callId, state) {
+  log.trace(">>> updateCallState for " + callId + " state: " + state);
   if (state === "started") {
     savePopupLoader(callId);
   }
